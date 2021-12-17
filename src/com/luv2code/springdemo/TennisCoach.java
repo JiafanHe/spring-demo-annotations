c03d6c93f3603cd664fc9e3c21d75aa09bf8dd31
JiafanHe <jiafan453@gmail.com>
2021-12-17 11:53:48 -0500

    Scope Annotation

--- src/com/luv2code/springdemo/TennisCoach.java
+++ src/com/luv2code/springdemo/TennisCoach.java
@@ -4,7 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
-@Component("thatSillyCoach")  //the bean "id"
+@Component  //the bean "id"
 public class TennisCoach implements Coach {
 	
 	@Autowired
